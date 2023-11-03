3b5bcf72b9279bdf699bb543b696fd2c9df43fd3
Mingyang Gao <gaomingyangcn@gmail.com>
2023-11-02 22:46:57 -0600

    完成timestamp service

--- timestamp-microservice/README.md
+++ timestamp-microservice/README.md
@@ -1,3 +1,15 @@
 # Timestamp Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+## Online Demo:
+[Timestamp Microservice](https://boilerplate-project-timestamp.gaomingyangcn.repl.co/)
+
+## run
+```
+node index.js
+```
+
+use `Pug` template engine
+
+```
+npm install pug --save
+```
--- timestamp-microservice/index.js
+++ timestamp-microservice/index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -13,9 +15,49 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+//index page data
+app.get('/', (req, res) => {
+  let project_url = 'https://boilerplate-project-timestamp.gaomingyangcn.repl.co';
+
+  let currentDate = new Date();
+  let year = currentDate.getFullYear();
+  let month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 加1是因为月份从0开始，使用padStart来确保两位数字
+  let day = String(currentDate.getDate()).padStart(2, '0');
+
+  let date_string = `${year}-${month}-${day}`;
+
+  let timestampMilliseconds = new Date().getTime(); // 获取当前时间戳，以毫秒为单位
+  // const timestampSeconds = Math.floor(currentTimestampMilliseconds / 1000); // 转换为秒级的时间戳
+
+  let outputObj = {unix:timestampMilliseconds,utc:currentDate.toUTCString()}
+  let output = JSON.stringify(outputObj);
+
+
+  res.render('index', { project_url: project_url,date_string: date_string,timestampMilliseconds: timestampMilliseconds,output:output })
+})
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+
+app.get("/api/:timestr",function(req,res){
+  var timestr = req.params.timestr
+  var timestamp,utcString
+
+  if (/^\d+$/.test(timestr)) {
+    //传入纯数字，是时间戳，Date解析这个需要用整型
+    timestamp=parseInt(timestr, 10);
+    utcString = new Date(timestamp).toUTCString();
+  }else{
+    //传入是时间字符串，直接解析字符串
+    timestamp = new Date(timestr).getTime();
+    utcString = new Date(timestamp).toUTCString();
+  }
+
+  let output = {unix:timestamp,utc:utcString}
+  res.json(output)
 });
 
 
@@ -27,6 +69,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT|3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
