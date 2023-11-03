a915a5ca9165bf078e773754e5b5bdb3481e53bd
Mingyang Gao <gaomingyangcn@gmail.com>
2023-11-03 15:42:36 -0600

    view

--- exercisetracker/README.md
+++ exercisetracker/README.md
@@ -1,3 +1,24 @@
 # Exercise Tracker
 
-This is the boilerplate for the Exercise Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
+## deploy and run
+
+
+run
+```
+node index.js
+```
+
+
+
+
+
+
+## note
+```
+npm install
+npm install ejs body-parser mongoose
+```
+
+
+
+https://github.com/gaomingyang/fcc-backend/tree/main/exercisetracker
--- exercisetracker/index.js
+++ exercisetracker/index.js
@@ -3,12 +3,35 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
+app.set('view engine', 'ejs');
+
+app.use(cors())
+const myLogger = function(req,res,next){
+  var logstr = req.method + " " + req.path + " - " + req.ip;
+  console.log(logstr)
+  next();
+}
+app.use(myLogger)
+
+// 使用 body-parser 中间件来解析 POST 请求的数据
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html')
+// });
+
+app.get('/', (req, res) => {
+  let base_url = process.env.BASE_URL;
+  res.render('index', { base_url : base_url});
+});
 
 
 
